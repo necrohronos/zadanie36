022b3da7809e9df9ddf7c8b443df7d5436e386f6
Bogdan Ciechomski <bogdan.ciechomski@gmail.com>
2018-07-09 18:02:19 +0200

    fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function myNumbers() {
     var x = prompt("wpisz pierwszą liczbę");
     var y = prompt("wpisz drugą liczbę");
-    document.write(x == 20 || y == 20 || x + y <= 20);
+   return x == 20 || y == 20 || x + y <= 20;
 }
+document.getElementById("logika").innerHTML = myNumbers();
